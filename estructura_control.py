fd45bfec68186be723d67f186b7f9e93649bfc6b
adrianluciano1992-cell <adrianluciano1992@gmail.com>
2026-01-22 11:43:18 -0400

    Update estructura_control.py

--- estructura_control.py
+++ estructura_control.py
@@ -8,5 +8,7 @@ print (f"suma: 10 + 3 = {10 + 3}")
 print (f"resta: 10 - 3 = {10 - 3})
 print (f " multiplicacion: 10 * 3 = {10 *3})
 print (f " division: 10 / 3: {10 / 3})
+print (f" modulo: 10 % 3: {10 % 3})
+print(f"esponente: 10 ** 3 ={10 ** 3}")
 
 
