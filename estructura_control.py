047dbf60249de95ee7cecc24b1bfcc1d3da1bb12
adrianluciano1992-cell <adrianluciano1992@gmail.com>
2026-01-22 12:52:20 -0400

    Update estructura_control.py

--- estructura_control.py
+++ estructura_control.py
@@ -18,4 +18,60 @@ print(f"igualda: 10 == 3 = {10 == 3}")
 print(f" desigualdad: 10 != 3 = {10 != 3})
 print (f " mayor que : 10 > 3 = {10 > 3})
 print (f " nenor que : 10 < 3 = {10 < 3})
+print (f "mayor o igual: 10 >= 3 = {10 >= 3}")
+print (f "menor o igual: 10 <= 3 = {10 <= 3})
+
+# operadores logicos: son comunes a la gran mayoria de los otros lenguajes 
+print (f"and && : 10 + 3 == 13 and 5 - 3 == 2 es {10 + 3 == 13 and 5 - 3 == 2}")
+
+# or nos pide que aunque sea una verdadera uan de las condiciones  
+print (f"OR || : 10 + 3 == 13 or 5 - 3 == 2 es {10 + 3 == 13 and 5 - 3 == 2}") 
+
+# agregando el not no seria verdadero en caso de que lo que esta alli no se cumpliera 
+print(f " NOT ! : not 10 + 3 == 14 es {not 10 + 3 == 14}")
+
+"""
+se usan en conjunto con el valor de las variable
+operadores de asignacion
+"""
+
+my_number = 11 # asignacion
+
+print(my_number)
+my_number += 1 # suma y asignacion
+
+print(my_number)
+my_number -= 3 # resta y asignacion
+
+print(my_number)
+my_number /= 4 # division y asignacion
+
+print(my_number)
+my_number *= # multiplicacion y asignacion
+
+print(my_number)
+my_number %= 3 # modulo y asignacion
+
+print(my_number)
+my_number //= 4 # division entera y asignacion 
+
+# operadores de identidad: nos sirven para comprar valores . valores de la posicion de memoria
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
