c0fa4313fc7933001c95a0fb533dd2171571a7e6
adrianluciano1992-cell <adrianluciano1992@gmail.com>
2026-01-22 09:50:51 -0400

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,26 @@
+# https://python.org/
+
+# comentario en una linea
+
+"""
+esto es un comentario
+en varias lineas 
+se coloca 3 comillas para python
+"""
+'''
+esto es un comentario
+en varias lineas 
+se coloca 3 comillas para python
+'''
+my_variable = "my variable"
+my_variable = " my nueva variable"
+
+MY_CONSTAST = "mi constante" # por convencion
+
+'''
+lo que viene a continuacion son tipos de datos primitivos son 4
+pero aqui esta mostrado en pyton
+'''
+
+
 print("¡Hola! Ya funciona mi GitHub.")
