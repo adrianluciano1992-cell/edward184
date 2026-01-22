4b3df29bd54d10275408e42f2c49a95b844f6bb9
adrianluciano1992-cell <adrianluciano1992@gmail.com>
2026-01-22 11:52:49 -0400

    Update estructura_control.py

--- estructura_control.py
+++ estructura_control.py
@@ -8,11 +8,14 @@ la f es interporacion
 print (f"suma: 10 + 3 = {10 + 3}")
 print (f"resta: 10 - 3 = {10 - 3})
 print (f " multiplicacion: 10 * 3 = {10 *3})
-print (f " division: 10 / 3: {10 / 3})
-print (f" modulo: 10 % 3: {10 % 3})
+print (f " division: 10 / 3 = {10 / 3})
+print (f" modulo: 10 % 3 = {10 % 3})
 print(f"esponente: 10 ** 3 ={10 ** 3}")
 print (f"division entera: 10 // 3 = {10 // 3}")
 
 # operadores de comparacion (nos sirve para comparar valores de variables 
-print(f"igualda: 10 == 3 {10 == 3}")
+print(f"igualda: 10 == 3 = {10 == 3}")
+print(f" desigualdad: 10 != 3 = {10 != 3})
+print (f " mayor que : 10 > 3 = {10 > 3})
+print (f " nenor que : 10 < 3 = {10 < 3})
 
