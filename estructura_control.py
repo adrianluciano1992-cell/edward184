c81d736fa8621126639550e63ba62e187caa6149
adrianluciano1992-cell <adrianluciano1992@gmail.com>
2026-01-22 11:39:32 -0400

    Update estructura_control.py

--- estructura_control.py
+++ estructura_control.py
@@ -1,1 +1,12 @@
+"""
+operadores
+"""
+
+# operadores aritmeticos
+
+print (f"suma: 10 + 3 = {10 + 3}")
+print (f"resta: 10 - 3 = {10 - 3})
+print (f " multiplicacion: 10 * 3 = {10 *3})
+print (f " division: 10 / 3: {10 / 3})
+
 
